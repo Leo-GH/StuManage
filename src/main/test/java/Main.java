326455cfc813c4b3e817179c53e5d49ed5bd9f9a
Leo <2423493653@qq.com>
2019-03-29 17:03:49 +0800

    Version 1.0

---
+++ src/main/test/java/Main.java
@@ -0,0 +1,13 @@
+import db.DbConnect;
+
+/**
+ * @ClassName: Main
+ * @Author: Leo
+ * @Description:
+ * @Date: 2019/3/28 21:27
+ */
+public class Main {
+    public static void main(String[] args) {
+        DbConnect.getConnection();
+    }
+}
